f4e75ee52bb476d7d3595d433a616f7640366539
Juan Latorre <juanlatorre@protonmail.com>
2018-12-19 10:20:35 -0300

    implementando login

--- index.php
+++ index.php
@@ -1,6 +1,32 @@
 <?php
 include_once('sql/config.php');
 $query = mysqli_query($db, "SELECT * FROM Taller");
+
+session_start();
+if (session_id() == '' || !isset($_SESSION['login'])) {
+	 include 'dist/login.html';
+} else { 
+	echo 'Ya estás logeado.';
+}
+
+if ($_SERVER["REQUEST_METHOD"] == "POST") {
+	$usuario = mysqli_real_escape_string($db,$_POST['usuario']);
+	$clave	 = mysqli_real_escape_string($db,$_POST['clave']);
+	
+	$sql = "SELECT AdministradorID FROM administradores WHERE usuario = '$usuario' and clave = '$clave'";
+	$result = mysqli_query($db,$sql);
+	$row = mysqli_fetch_array($result,MYSQLI_ASSOC);
+	$count = mysqli_num_rows($result);
+	
+	if($count == 1) {
+		session_register($usuario);
+		$_SESSION['login'] = $usuario;     
+			include 'dist/index.html';
+	      }else {
+	         $error = "Your Login Name or Password is invalid";
+		 }
+} 
+
 ?>
 
 <!DOCTYPE html>
@@ -10,10 +36,9 @@ $query = mysqli_query($db, "SELECT * FROM Taller");
 	<meta name="viewport" content="width=device-width, initial-scale=1">
 	<title>Talleres Cabo Blanco</title>
 	<link rel="stylesheet" href="dist/css/styles.css">
+	<script defer src="https://use.fontawesome.com/releases/v5.3.1/js/all.js"></script>
 </head>
 <body>
-
-<?php include 'dist/index.html'; ?>
 
 </body>
 </html>
--- sql/config.php
+++ sql/config.php
@@ -1,12 +1,10 @@
 <?php
 $dbhost="localhost";
-$dbuser="root";
-$dbpass="root";
-$dbname="talleres";
+$dbuser="cabokhwv_admin";
+$dbpass="khl0695";
+$dbname="cabokhwv_talleres";
 
 $db = mysqli_connect($dbhost, $dbuser, $dbpass, $dbname) or die ("Error conectando a la base de datos.");
 mysqli_query($db, "SET NAMES 'utf8'");
-mysqli_select_db($db, "talleres") or die (mysql_error());
-
-$target_dir = "../dist/uploads/";
+mysqli_select_db($db, $dbname) or die (mysql_error());
 ?>
