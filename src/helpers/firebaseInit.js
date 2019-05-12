21493f3b31f18122764cd5487fdf4a3856407e9f
Juan Latorre <juanlatorre@protonmail.com>
2019-05-12 11:45:28 -0400

    separado api key

--- src/helpers/firebaseInit.js
+++ src/helpers/firebaseInit.js
@@ -2,16 +2,7 @@ import firebase from "firebase/app";
 import "firebase/auth";
 import "firebase/firestore";
 import "firebase/firebase-functions";
-
-const firebaseConfig = {
-  apiKey: "AIzaSyAMf4SKxT5eoI6MotAyFx6a1HX2PnQxnak",
-  authDomain: "talleres-16e00.firebaseapp.com",
-  databaseURL: "https://talleres-16e00.firebaseio.com",
-  projectId: "talleres-16e00",
-  storageBucket: "talleres-16e00.appspot.com",
-  messagingSenderId: "912820578646",
-  appId: "1:912820578646:web:8fa5ff85f10ab8c1"
-};
+import firebaseConfig from "./credentials.js";
 
 firebase.initializeApp(firebaseConfig);
 
