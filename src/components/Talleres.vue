e676c4ea5ceecb4b839ace1f747fa4b22536ca94
Juan Latorre <juanlatorre@protonmail.com>
2019-05-12 20:41:10 -0400

    Add: borrar talleres

--- src/components/Talleres.vue
+++ src/components/Talleres.vue
@@ -1,5 +1,5 @@
 <template>
-  <div class="columns is-mobile">
+  <div class="columns is-mobile" v-if="talleres.length > 0">
     <div class="column is-6-mobile is-3-tablet" v-for="(taller, idx) in talleres" :key="idx">
       <div class="card">
         <div class="card-content" @click="abrirTaller(taller.id)">
@@ -16,7 +16,7 @@
           </p>
           <p class="card-footer-item">
             <span>
-              <a href="#">
+              <a href="javascript:void(0);" @click="eliminarTaller(taller.id)">
                 <i class="fas fa-trash"></i> Eliminar
               </a>
             </span>
@@ -25,29 +25,61 @@
       </div>
     </div>
   </div>
+  <Loader v-else/>
 </template>
 
 <script>
 import { db } from "@/helpers/firebaseInit.js";
+import Loader from "@/components/Loader.vue";
 
 export default {
+  components: {
+    Loader
+  },
   data() {
     return {
       talleres: []
     };
   },
   mounted: function() {
-    db.collection("talleres")
-      .get()
-      .then(querySnapshot => {
-        querySnapshot.forEach(doc => {
-          this.talleres.push(doc.data());
-        });
-      });
+    this.leerDatos();
   },
   methods: {
+    leerDatos() {
+      this.talleres = [];
+      db.collection("talleres")
+        .get()
+        .then(querySnapshot => {
+          querySnapshot.forEach(doc => {
+            this.talleres.push(doc.data());
+          });
+        });
+    },
     abrirTaller(id) {
       this.$router.push("/taller/" + id);
+    },
+    eliminarTaller(id) {
+      db.collection("talleres")
+        .where("id", "==", id)
+        .get()
+        .then(querySnapshot => {
+          querySnapshot.forEach(doc => {
+            db.collection("talleres")
+              .doc(doc.id)
+              .delete()
+              .then(() => {
+                this.$snackbar.open({
+                  duration: 3000,
+                  message: "El taller ha sido eliminado.",
+                  type: "is-success",
+                  position: "is-bottom-right",
+                  actionText: "Ok",
+                  queue: false
+                });
+                this.leerDatos();
+              });
+          });
+        });
     }
   }
 };
