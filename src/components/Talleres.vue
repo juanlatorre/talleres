8790a86e914b7390903e6eafa1f07df3f1b90d5d
Juan Latorre <juanlatorre@protonmail.com>
2019-05-16 01:03:13 -0400

    feat(talleres): show when there are no workshops

--- src/components/Talleres.vue
+++ src/components/Talleres.vue
@@ -1,31 +1,35 @@
 <template>
-  <div class="columns is-mobile" v-if="talleres.length > 0">
-    <div class="column is-6-mobile is-3-tablet" v-for="(taller, idx) in talleres" :key="idx">
-      <div class="card">
-        <div class="card-content" @click="abrirTaller(taller.id)">
-          <p class="title is-5">{{taller.nombre}}</p>
-          <p class="subtitle">{{fecha(taller.fecha.seconds)}}</p>
+  <div>
+    <div class="columns is-mobile" v-if="talleres.length > 0">
+      <div class="column is-6-mobile is-3-tablet" v-for="(taller, idx) in talleres" :key="idx">
+        <div class="card">
+          <div class="card-content" @click="abrirTaller(taller.id)">
+            <p class="title is-5">{{taller.nombre}}</p>
+            <p class="subtitle">{{fecha(taller.fecha.seconds)}}</p>
+          </div>
+          <footer class="card-footer">
+            <p class="card-footer-item">
+              <span>
+                <a href="javascript:void(0);" @click="editarTaller(taller.id)">
+                  <i class="fas fa-pencil-alt"></i> Editar
+                </a>
+              </span>
+            </p>
+            <p class="card-footer-item">
+              <span>
+                <a href="javascript:void(0);" @click="eliminarTaller(taller.id)">
+                  <i class="fas fa-trash"></i> Eliminar
+                </a>
+              </span>
+            </p>
+          </footer>
         </div>
-        <footer class="card-footer">
-          <p class="card-footer-item">
-            <span>
-              <a href="javascript:void(0);" @click="editarTaller(taller.id)">
-                <i class="fas fa-pencil-alt"></i> Editar
-              </a>
-            </span>
-          </p>
-          <p class="card-footer-item">
-            <span>
-              <a href="javascript:void(0);" @click="eliminarTaller(taller.id)">
-                <i class="fas fa-trash"></i> Eliminar
-              </a>
-            </span>
-          </p>
-        </footer>
       </div>
     </div>
+    <div class="container has-text-centered" v-else>
+      <h4 class="subtitle is-5">No hay ningún taller.</h4>
+    </div>
   </div>
-  <Loader v-else/>
 </template>
 
 <script>
